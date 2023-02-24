cc3cf31136e8d7cbc79026d7f32e807848ffe8f3
“Edward <eminaya49@gmail.com>
2023-02-24 17:39:36 -0600

    completed all the conditionals

--- employee_app.rb
+++ employee_app.rb
@@ -32,43 +32,67 @@ employees << employee2
 puts "EMPLOYEES (#{employees.length} TOTAL)"
 p employees
 
-# FIRST PROMPT FOR EACH OPTION
-puts "[C]reate [R]ead [U]pdate [D]elete [Q]uit: "
-user_entry = gets.chomp.upcase
+while true
+  # FIRST PROMPT FOR EACH OPTION
+  puts "[C]reate [R]ead [U]pdate [D]elete [Q]uit: "
+  user_entry = gets.chomp.upcase
 
-# CONDITIONAL FOR EACH OPTION
-if user_entry == "C" # Create a new entry
-  new_entry = {}
-  puts "First Name: "
-  first_name = gets.chomp
-  puts "Last Name: "
-  last_name = gets.chomp
-  puts "Salary: "
-  salary = gets.chomp.to_i
-  new_entry[:id] = employees.length + 1
-  new_entry[:first_name] = first_name
-  new_entry[:last_name] = last_name
-  new_entry[:salary] = salary
-  new_entry[:active] = true
-  new_employee = Employee.new(new_entry)
-  employees << new_employee
-elsif user_entry == "R" # Enter an employee id# and print their information
-  puts "Enter Employee ID: "
-  entry_id = gets.chomp.to_i
-  puts "First Name: #{employees[entry_id - 1].first_name}"
-  puts "Last Name: #{employees[entry_id - 1].last_name}"
-  puts "Salary: $#{employees[entry_id - 1].salary}"
-  puts "Active?: #{employees[entry_id - 1].active}"
-elsif user_entry == "U" # Enter an employee id# and update their active status
-  puts "Enter Employee ID: "
-  entry_id = gets.chomp.to_i
-  puts "First Name: #{employees[entry_id - 1].first_name}"
-  puts "Last Name: #{employees[entry_id - 1].last_name}"
-  puts "Salary: $#{employees[entry_id - 1].salary}"
-  puts "Active?: #{employees[entry_id - 1].active}"
-  puts "Update Active Status enter 'fired' or 'reinstate': "
-  status update = gets.chomp
-elsif user_entry == "D" # Enter an employee id# and delete their entry
-elsif user_entry == "Q" # Quit application
-else
+  # CONDITIONAL FOR EACH OPTION
+  if user_entry == "C" # Create a new entry
+    new_entry = {}
+    puts "First Name: "
+    first_name = gets.chomp
+    puts "Last Name: "
+    last_name = gets.chomp
+    puts "Salary: "
+    salary = gets.chomp.to_i
+    new_entry[:id] = employees.length + 1
+    new_entry[:first_name] = first_name
+    new_entry[:last_name] = last_name
+    new_entry[:salary] = salary
+    new_entry[:active] = true
+    new_employee = Employee.new(new_entry)
+    employees << new_employee
+  elsif user_entry == "R" # Enter an employee id# and print their information
+    puts "Enter Employee ID: "
+    entry_id = gets.chomp.to_i
+    puts "First Name: #{employees[entry_id - 1].first_name}"
+    puts "Last Name: #{employees[entry_id - 1].last_name}"
+    puts "Salary: $#{employees[entry_id - 1].salary}"
+    puts "Active?: #{employees[entry_id - 1].active}"
+  elsif user_entry == "U" # Enter an employee id# and update their active status
+    puts "Enter Employee ID: "
+    entry_id = gets.chomp.to_i
+    puts "First Name: #{employees[entry_id - 1].first_name}"
+    puts "Last Name: #{employees[entry_id - 1].last_name}"
+    puts "Salary: $#{employees[entry_id - 1].salary}"
+    puts "Active?: #{employees[entry_id - 1].active}"
+    puts "Update Active Status enter 'fired' or 'reinstate': "
+    status_update = gets.chomp
+    if status_update.downcase == "fired"
+      employees[entry_id - 1].active = false
+    elsif status_update.downcase == "reinstate"
+      employees[entry_id - 1].active - true
+    end
+    puts "First Name: #{employees[entry_id - 1].first_name}"
+    puts "Last Name: #{employees[entry_id - 1].last_name}"
+    puts "Salary: $#{employees[entry_id - 1].salary}"
+    puts "Active?: #{employees[entry_id - 1].active}"
+  elsif user_entry == "D" # Enter an employee id# and delete their entry
+    puts "Enter Employee ID: "
+    entry_id = gets.chomp.to_i
+    puts "First Name: #{employees[entry_id - 1].first_name}"
+    puts "Last Name: #{employees[entry_id - 1].last_name}"
+    puts "Salary: $#{employees[entry_id - 1].salary}"
+    puts "Active?: #{employees[entry_id - 1].active}"
+    puts "Are you sure you want to delete employee? Type 'Yes' or 'No'"
+    delete_decision = gets.chomp
+    if delete_decision.downcase == "yes"
+      employees.delete_at(entry_id - 1)
+    end
+  elsif user_entry == "Q" # Quit application
+    puts "Thank you!"
+    break
+  else
+  end
 end
